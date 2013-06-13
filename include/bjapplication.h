bb1003131b29149a6c18f24f6c97110fc3b8613c
vidal.guillaume@gmail.com <vidal.guillaume@gmail.com@ef5c3b63-b276-856c-0798-46a68a3c061f>
2013-06-13 09:50:31 +0000

    application

--- include/bjapplication.h
+++ include/bjapplication.h
@@ -1,9 +1,13 @@
 #pragma once
 
-#include "QCoreApplication"
+#include "QApplication"
 
-class BJackApplication : public QCoreApplication {
+class BJackApplication : public QApplication {
 public:
 	BJackApplication(int argc, char *argv[]);
 	virtual ~BJackApplication();
+protected:
+	virtual bool	event ( QEvent * e );
+private:
+	QWidget* m_widget;
 };
