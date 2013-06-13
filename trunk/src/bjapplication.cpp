ed1a538421b9f72fcc0f4071185a6a2f03fed26c
vidal.guillaume@gmail.com <vidal.guillaume@gmail.com@ef5c3b63-b276-856c-0798-46a68a3c061f>
2013-06-13 08:44:29 +0000

    bj application inheritance

---
+++ trunk/src/bjapplication.cpp
@@ -0,0 +1,8 @@
+#include "bjackapplication.h"
+
+BJackApplication::BJackApplication(int argc, char *argv[]) : QCoreApplication(argc, argv) {
+
+}
+
+virtual ~BJackApplication::BJackApplication() {
+}
