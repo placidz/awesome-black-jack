2f63d2b88d95c839a4708cc00809f35877fb09a1
vidal.guillaume@gmail.com <vidal.guillaume@gmail.com@ef5c3b63-b276-856c-0798-46a68a3c061f>
2013-06-13 09:50:31 +0000

    application

--- trunk/src/bjapplication.cpp
+++ trunk/src/bjapplication.cpp
@@ -1,9 +1,30 @@
 #include "bjapplication.h"
-
-
-BJackApplication::BJackApplication(int argc, char *argv[]) : QCoreApplication(argc, argv) {
-	
+#include <iostream>
+#include "QTextStream"
+#include <QKeyEvent>
+#include <QWidget>
+BJackApplication::BJackApplication(int argc, char *argv[]) : QApplication(argc, argv) {
+	QTextStream stream(stdout);
+	stream << QString::fromLatin1("Welcome to the Awesome black Jack") << endl;
+	m_widget = new QWidget();
+	m_widget->show();
+	installEventFilter(m_widget);
 }
 
 BJackApplication::~BJackApplication() {
+}
+
+bool BJackApplication::event ( QEvent * e ) {
+	switch(e->type())
+	{
+	case QEvent::KeyRelease: {
+		QTextStream stream(stdout);
+		QKeyEvent* keyE = dynamic_cast<QKeyEvent*>(e);
+		if(keyE)
+			stream << QString::fromLatin1("you pressed ") << keyE->text() << endl;
+							 }
+	default:
+		break;
+	}
+	return QCoreApplication::event(e);
 }
